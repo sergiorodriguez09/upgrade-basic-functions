d2cb51ee40d84ee825b37ac1c6530ac5cb65074f
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-06 22:14:51 +0200

    Iteracion 1

--- Iteration1.js
+++ Iteration1.js
@@ -7,6 +7,9 @@ function sum(numberOne , numberTwo) {
     else if (numberTwo > numberOne) {
         console.log("el numero mas grande es", numberTwo);
     }
+    else if (numberOne == numberTwo) {
+        console.log("los dos numeros son iguales");
+    }
   }
 
 sum(100, 25);
